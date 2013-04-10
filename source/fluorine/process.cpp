cd3796e6a95d73125c04dff4e3c33833a9df1844
snowmantw <snowmantw@gmail.com>
2013-04-11 02:32:28 +0800

    Process with Next; working on Run

--- source/fluorine/process.cpp
+++ source/fluorine/process.cpp
@@ -3,12 +3,16 @@
 
 #include "process.hpp"
 
+#include <queue>
+
 #include <node.h>
 
 using namespace v8;
 using namespace node;
+using namespace std;
 
 Process::Process(){};
+
 Process::~Process(){};
 
 Persistent<Function> Process::constructor;
@@ -24,7 +28,12 @@ void Process::Init()    // -- Don't forget the class name.
 
     // -- NewSymbol, not only New.
     tpl->SetClassName(String::NewSymbol("Process"));
+
     tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+    // -- Register functions on the prototype.
+    tpl->PrototypeTemplate()->Set(String::NewSymbol("next")
+        ,FunctionTemplate::New(Process::Next)->GetFunction() );
 
     constructor = Persistent<Function>::New(tpl->GetFunction());
 }
@@ -42,6 +51,15 @@ Handle<Value> Process::New(const Arguments& args)
     HandleScope scope;
 
     Process* process = new Process();
+
+    // -- Note register functions on the prototype is not a part of constructor.
+    //    At least not in this constructor C++ function.
+    //    It need to use `PrototypeTemplate`, @see the Init member function.
+    // -- But other inner data still can be setup here.
+
+    // -- ?? Don't know how can I do deconstructor to pevent the dangling pointer of the queue.
+    process->m_pqueue = new queue<Persistent<Function> >();
+
     process->Wrap(args.This()); 
 
     return args.This();
@@ -62,4 +80,58 @@ Handle<Value> Process::NewInstance(const Arguments& args)
     Local<Object> instance = constructor->NewInstance(argc, argv);
 
     return scope.Close(instance);
+}
+
+/**
+ * Define the next step this process should execute.
+ * Every step in the Process will receive previous one's result as it's only one argument
+ * before the tuple extracting-packeting implemeted. -- TODO
+ * 
+ * So user should pass a fuction can surely accept the right result of previous one.
+ *
+ * @param args Need pass a Javascript function in as the next step.
+ * @return The Process instance itself.
+ */
+Handle<Value> Process::Next(const Arguments& args)
+{
+    HandleScope scope;
+
+    // -- Unlike `Local<Function>::Cast`, use `Persistent<Function>::New` to cast it
+    // -- !! Note the function passin function is `Handle<Function>`, it need to be casted manually before `New` it,
+    //       or compiler will complain like : "v8.h:225:5: Error：... from 「v8::Value*」 to 「v8::Function*」 ... [-fpermissive]
+
+    Persistent<Function> step = Persistent<Function>::New(Handle<Function>::Cast(args[0]));
+
+    // -- Access "this" instance via unwrap it from arguments.
+    Process* process = ObjectWrap::Unwrap<Process>(args.This());
+
+    // -- Don't forget the arror, you're writting C++ !
+    process->m_pqueue->push(step);
+
+    return args.This();
+}
+
+/**
+ * Execute one step of the process with a result from previous step.
+ * The passed in result will become the process' current result, until another run update it.
+ * 
+ * @param args Pass any one parameter to let the function execute.
+ * @return Undefined, nothing.
+ */
+Handle<Value> Process::Run(const Arguments& args)
+{
+    HandleScope scope;
+
+    Process* process = ObjectWrap::Unwrap<Process>(args.This());
+    process->m_result = Persistent<Handle<Value> >::New(args[0]);
+    Persistent<Function> step = process->m_pqueue->front();
+    process->m_pqueue->pop();
+
+    Local<Value> adata = args.Data();
+
+    // Steps should execute under the plain context; 
+    // they're expecting become closure already.
+    step->Call(Object::New(), args.Length(), adata );
+
+    return scope.Close(Undefined());
 }
--- source/fluorine/process.hpp
+++ source/fluorine/process.hpp
@@ -4,6 +4,8 @@
 #ifndef FLUORINE_PROCESS_H
 #define FLUORINE_PROCESS_H
 
+#include <queue>
+
 #include <node.h>
 
 class Process : public node::ObjectWrap
@@ -21,6 +23,16 @@ private:
     static v8::Persistent<v8::Function> constructor;
 
     static v8::Handle<v8::Value> New(const v8::Arguments& args);
+
+    // ----
+
+    static v8::Handle<v8::Value> Next(const v8::Arguments& args);
+    static v8::Handle<v8::Value> Run(const v8::Arguments& args);
+
+    // ----
+
+    std::queue<v8::Persistent<v8::Function> >* m_pqueue;
+    v8::Persistent<v8::Handle<v8::Value> > m_result;
 };
 
 #endif
